774b7e945e3c816abeaa6236e50b00dc53d26bea
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-12 20:24:16 +0100

    First commit

---
+++ SetupClient.java
@@ -0,0 +1,7 @@
+/**
+ * A client which enables a user to setup new quizzes on a QuizService.
+ *
+ * In addition, a user can terminate an active and register as a new user.
+ */
+public interface SetupClient {
+}
