92395e7f430ab8d9db23bc4b9b110716bc1bdd32
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-12 12:14:40 +0100

    First commit, contains getters only. I suspect setters will not be required.

---
+++ Question.java
@@ -0,0 +1,28 @@
+/**
+ * A question for the quiz.
+ * Comprises a question, four multiple choice answers, 
+ * and an order number indicating the order this question asked in the quiz.
+ */
+public interface Question {
+
+	/**
+	 * @return the order in which this question is asked in the quiz.
+	 */
+	int getIndex();
+	
+	/**
+	 * @return the question itself.
+	 */
+	String getQuestion();
+	
+	/**
+	 * @return possibleAnswers for the quiz, the multiple choice part.
+	 */
+	String[] getPossIbleAnswers();
+	
+	/**
+	 * @return an int which identifies the correct answer from the array of possible answers.
+	 * The int represent the index of the array where the correct answer lies.
+	 */
+	int getCorrectAnswer();
+}
