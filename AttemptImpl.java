72bd981aee769f362756b889ebed290851eec6bb
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-13 22:51:54 +0100

    First commit, just a constructor method

---
+++ AttemptImpl.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+/**
+ * Implements the interface Attempt.
+ */
+public class AttemptImpl implements Attempt {
+	/**
+	 * The userName of the Player who attempted the quiz.
+	 */
+	String userName;
+	/**
+	 * The score of the player who attempted the quiz.
+	 */
+	double score;
+	/**
+	 * The date when a Player attempted the quiz.
+	 */
+	Calendar date;
+	 
+	 public AttemptImpl(String userName, double score, Calendar date) {
+	 	 this.userName = userName;
+	 	 this.score = score;
+	 	 this.date = date;
+	 }
+}
