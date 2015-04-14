c271f08567e43edb8c49746964822361a9c551fa
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-15 00:03:33 +0100

    More tests added.

--- QuizTest.java
+++ QuizTest.java
@@ -3,9 +3,58 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import java.util.GregorianCalendar;
 import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Tests the class QuizImpl and the interface QuizTest.
  */
 public class QuizTest {
+	
+	Quiz testQuiz;
+	//A Quiz object for use in the following tests.
+	
+	/**
+	 * Instantiates testQuiz.
+	 */
+	@Before
+	public void buildUp() {
+		List<Question> questions = ArrayList<Question>();
+		questions.add(new QuestionImpl("Where is the Hagia Sophia?", new String[] {"Constantinople", "Rome". "Athens", "Antioch"}, 1);
+		testQuiz =  new QuizImpl(questions, "Justinian", "The Byzantine Quiz");
+	}
+	
+	/**
+	 * Tests setQuizId() and getQuizId().
+	 * 
+	 * Should return 1.
+	 */
+	@Test
+	public void shouldReturnQuizId1() {
+		testQuiz.setQuizId(1);
+		assertEquals(1, testQuiz.getQuizId());
+	}
+	
+	/**
+	 * Tests terminate() and isTerminated().
+	 *
+	 * Should return true after quiz has been terminated.
+	 */
+	@Test
+	public void shouldReturnTrueAfterTerminated() {
+		testQuiz.terminate();
+		assertTrue(testQuiz.isTerminated());
+	}
+	
+	/**
+	 * Tests addNewQuestion().
+	 *
+	 * testQuiz contains one question already. 
+	 * After a question has been added, the size of the List of Questions should be 2.
+	 */
+	@Test
+	public void shouldCreateQuestionListOfSize2() {
+		testQuiz.addNewQuestion("Who founded Constantinople?", new String[] {"Valens", "Justin", "Diocletian", "Constantine"}, 4);
+		assertEquals(2, testQuiz.questions.size());
+	}
 }
