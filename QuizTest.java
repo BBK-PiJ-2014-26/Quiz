23d314d8138db1a7e578bcb927ed238a7492749d
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-14 23:32:36 +0100

    First commit

---
+++ QuizTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+
+/**
+ * Tests the class QuizImpl and the interface QuizTest.
+ */
+public class QuizTest {
+}
