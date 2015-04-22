07977720ca73a0080d46226040efc656ebee6b8c
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-22 15:24:49 +0100

    first commit

---
+++ LeaderboardImpl.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+/**
+ * Implements the interface Leaderboard.
+ * Extends an ArrayList, but overrides the add method to ensure Attempts are organised by score.
+ */
+public class LeaderboardImpl implements Leaderboard extends ArrayList {
+}
