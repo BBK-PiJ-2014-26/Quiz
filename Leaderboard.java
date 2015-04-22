0d4cc2678db5e5b407c58d84b51fc94eb8c15daf
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-22 15:21:47 +0100

    First commit, no methods, will add as necessary.

---
+++ Leaderboard.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * The Leaderboard class organises Attempts at a quiz using a List structure.
+ * Attempts are organised by score with the highest score occupying the smallest index 
+ * and the lowest score occupying the largest index. For example, there are two scores 8 and 4, 
+ * 8 would occupy index 0, and 4 would occupy index 1.
+ */
+public interface Leaderboard extends List {
+
+}
+ 
