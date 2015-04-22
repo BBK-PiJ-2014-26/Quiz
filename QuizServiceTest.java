31da71d06a74ee702d169a55fed13b1e85e5ddb5
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-22 23:39:20 +0100

    First draft of @Before

--- QuizServiceTest.java
+++ QuizServiceTest.java
@@ -7,4 +7,20 @@ import org.junit.Before;
 /**
 * Tests the interface QuizService.
 */
-public class QuizServiceTest {}
+public class QuizServiceTest {
+
+		//A QuizService object for use in the following tests.
+		QuizService testService;
+		
+		/**
+		 * Initialises a QuizService.
+		 * Adds three uniqe players.
+		 */
+		@Before
+		public void buildUp() {
+			testService = new QuizServer();
+			testService.registerNewPlayer("Justinian");
+			testService.registerNewPlayer("Valens");
+			testService.registerNewPlayer("Theodora");
+		}
+}
