84561b7a9c09c5bb95e7780ef52234a47763fcf1
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-22 22:20:45 +0100

    First commit

---
+++ QuizServiceTest.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+* Tests the interface QuizService.
+*/
+public class QuizServiceTest {}
