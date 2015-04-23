15db8e18c32d3f1e380cac3195350fbbcc0f3f8e
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-23 23:27:23 +0100

    added impl for addNewQuiz()

--- QuizServer.java
+++ QuizServer.java
@@ -1,17 +1,34 @@
 import java.util.TreeSet;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.LinkedList;
 
 /**
  * Implements the interface QuizService.
  */
-public class QuizServer implements QuizService {
+public class QuizServer extends UnicastRemoteObject implements QuizService {
 	/**
-	 * A TreeSet which holds all of the registered Players on the QuizService.
-	 * Uses a TreeSet because this enables quicker searching.
+	 * A TreeSet which holds registered Players on the QuizService.
+	 * Uses a TreeSet because userNames are supplied randomly so the TreeSet should provide efficient searching.
 	 */
 	private TreeSet<Player> players;
+	/**
+	 * A LinkedList which holds Quizzes.
+	 */
+	private LinkedList<Quiz> quizzes;
+	/**
+	 * An int counter for tracking quizIds.
+	 * Holds the next unique id. Each call of nextId() increments the counter.
+	 */
+	private int quizIdCounter;
+	 
 	
-	public QuizServer() {
-		this.players = TreeSet<Player>(new PlayerComparator());
+	public QuizServer() throws RemoteException {
+		players = new TreeSet<Player>(new PlayerComparator());
+		quizzes = new LinkedList<Quiz>();
+		//Initialises the unique Id counter to 0.
+		quizIdCounter = 1;
 	}
 
 	public void registerNewPlayer(String userName) 
@@ -27,7 +44,6 @@ public class QuizServer implements QuizService {
 			} else {
 				players.add(new PlayerImpl(userName));
 			}
-		}
 	}
 	
 	public boolean userNameExists(String userName) {
@@ -40,5 +56,55 @@ public class QuizServer implements QuizService {
 		}
 		return result;
 	}
+	
+	public List<Quiz> getAllActiveQuizzes() throws RemoteException {
+		return new LinkedList<Quiz>();
+	}
+	
+	public List<Question> playQuiz(String userName, int quizId) throws RemoteException {
+		return new LinkedList<Question>();
+	}
+	
+	public void addNewAttempt(int quizId, String userName, int score) 
+		throws RemoteException, IllegalArgumentException {}
 		
+	public int addNewQuiz(Quiz quiz) 
+		throws RemoteException, IllegalArgumentException, NullPointerException {
+			//Checks if quiz is null.
+			//If true, an exception is thrown.
+			if (quiz.equals(null)) {
+				throw new NullPointerException();
+			//Checks if the author is a registered Player.
+			//If false, an exception is thrown.
+			} else if (!userNameExists(quiz.getAuthor())) {
+				throw new IllegalArgumentException();
+			} else {
+				//Calls nextId() to return the next unique quizId.
+				int quizId = nextId();
+				quiz.setQuizId(quizId);
+				quizzes.add(quiz);
+				return quizId;
+			}
+	}
+	
+	/**
+	 * A method which controls the quizIdCounter.
+	 * Returns the current value of quizIdCounter,
+	 * then increments the counter by 1.
+	 * Has private access because the method should only be accessed within addNewQuiz(Quiz).
+	 *
+	 * @return a unique quizId.
+	 */
+	private int nextId() {
+		int result = quizIdCounter;
+		quizIdCounter++;
+		return result;
+	}
+	
+	public void terminateQuiz(String userName, int quizId) 
+		throws RemoteException, IllegalArgumentException {}
+		
+	//private void flush() {}
+	
+	//private void read() {}
 }
