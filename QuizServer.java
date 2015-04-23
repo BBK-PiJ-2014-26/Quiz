efee37fd4c522626f0933c5d02780356455e0d86
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-23 01:18:04 +0100

    Implemented 2 methods

--- QuizServer.java
+++ QuizServer.java
@@ -1,5 +1,8 @@
 import java.util.TreeSet;
 
+/**
+ * Implements the interface QuizService.
+ */
 public class QuizServer implements QuizService {
 	/**
 	 * A TreeSet which holds all of the registered Players on the QuizService.
@@ -13,6 +16,29 @@ public class QuizServer implements QuizService {
 
 	public void registerNewPlayer(String userName) 
 		throws RemoteException, IllegalArgumentException, NullPointerException {
+			//Checks whether userName is null.
+			//If true, an exception is thrown.
+			if (userName.equals(null)) {
+				throw new NullPointerException();
+			//Checks whether userName has already been registered.
+			//If true, an exception is thrown.
+			} else if (userNameExists(userName)) {
+				throw new IllegalArgumentException();
+			} else {
+				players.add(new PlayerImpl(userName));
+			}
+		}
+	}
+	
+	public boolean userNameExists(String userName) {
+		boolean result = false;
+		//Creates a temporary Player object using the argument userName.
+		Player temp = new PlayerImpl(userName);
+		//Uses this temp Player to check if it's contained within the instance variable players.		
+		if (players.contains(temp)) {
+			result = true;
+		}
+		return result;
 	}
 		
 }
