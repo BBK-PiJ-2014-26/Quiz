15e9f0114e2c8e330acdaddbb6ffc602fffbeb4f
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-25 16:33:00 +0100

    Implementation complete-ish

--- SetupClientImpl.java
+++ SetupClientImpl.java
@@ -1,9 +1,16 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.List;
+
+/**
+ * Implements the interface SetupClient and extends QuizClientImpl.
+ */
 public class SetupClientImpl extends QuizClientImpl implements SetupClient {
 	
 	public void welcome() {
 		System.out.println("Welcome to Quizzes of the Ancients.");
 		System.out.print("\n" + "SETUP\n" + "\n");
-		selectWelcomeSelection();		
+		selectWelcomeOption();		
 	}
 	
 	/**
@@ -18,10 +25,11 @@ public class SetupClientImpl extends QuizClientImpl implements SetupClient {
 		//Checks if the user has selected 1.
 		//If true, runs login().
 		if (selection.equals("1")) {
+			String userName = requestUserName();
 			//Attempts to login the user.
 			//If login succeeds, the user proceeds.			
-			if (login(requestUserName())) {
-				chooseOption();
+			if (login(userName)) {
+				chooseOption(userName);
 			//If login fails, selectWelcomeOption is called again.
 			} else {
 				System.out.println("Username does not exist. Please try again.");
@@ -30,18 +38,19 @@ public class SetupClientImpl extends QuizClientImpl implements SetupClient {
 		//Checks if the user has selected 2.
 		//If true, runs register().
 		} else if (selection.equals("2")) {
+			String userName = requestUserName();
 			//Attempts to register a new Player.
 			//If registration succeeds, the user proceeds.
-			if (register(requestUserName())) {
+			if (register(userName)) {
 				System.out.println("There was an error. Please try again.");
 				selectWelcomeOption();
 			}
 		//Checks if user has selected 3.
 		//If true, the programme will terminate.
-		} else if (selection.equals("3") {}
+		} else if (selection.equals("3")) {
 		//If none of these conditions are true, an error has occurred.
 		} else {
-			selectWelcomeOption(requestUserName());
+			selectWelcomeOption();
 		}
 	}
 	
@@ -57,7 +66,7 @@ public class SetupClientImpl extends QuizClientImpl implements SetupClient {
 		return userName;
 	}
 	
-	public void chooseOption() {
+	public void chooseOption(String userName) {
 		System.out.println("Please select an option.");
 		System.out.print("\n" + "Type 1 to setup a new quiz.\n" + "Type 2 to terminate a quiz.\n");
 		System.out.print("Type 3 to exit.\n" + "Selection: ");
@@ -65,15 +74,85 @@ public class SetupClientImpl extends QuizClientImpl implements SetupClient {
 		String selection = sc.next();
 		//If the user has typed 1, calls setupNewQuiz().
 		if (selection.equals("1")) {
-			setupNewQuiz();
+			setupNewQuiz(userName);
 		//If user has typed 2, calls terminateQuiz().
 		} else if (selection.equals("2")) {
-			terminateQuiz();
+			terminateQuiz(userName);
 		//If user has typed 3, the programme completes its execution.
 		} else if (selection.equals("3")) {
 		//Otherwise there was an error input, so chooseOption is called again.
 		} else {
-			chooseOption();
+			chooseOption(userName);
+		}
+	}
+	
+	public void setupNewQuiz(String userName) {
+		System.out.print("Please enter the name of your quiz: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		List<Question> questions = new LinkedList(); 
+		requestQuestion(questions);
+		Quiz quiz = new QuizImpl(questions, userName, name);
+		try { 
+			int quizId = service.addNewQuiz(quiz);
+			System.out.println("Congratulations, you have made a quiz. Your quiz has ID " + quizId);
+		} catch (Exception e) {
+			System.out.println("There was an error. Please try again.");
+		}
+		chooseOption(userName);
+	}
+	
+	/**
+	 * Is called recursively until the user terminates.
+	 *
+	 * @param questions is the list of Question to which the new Question is to be added.
+	 */
+	private void requestQuestion(List<Question> questions) {
+		System.out.print("Please enter the text of your question: ");
+		Scanner sc = new Scanner(System.in);
+		String question = sc.next();
+		String[] possibleAnswers = new String[4];
+		//Initialises correctAnswer to 10 because this is not a valid answer.
+		//Should therefore not interfere with user input.
+		int correctAnswer = 10;
+		for (int i = 0; i < 4; i++) {
+			System.out.print("Please enter possible answer: ");
+			sc = new Scanner(System.in);
+			possibleAnswers[i] = sc.next();
+			System.out.print("Type y if this is the correct answer, type anything else if not: ");
+			sc = new Scanner(System.in);
+			String answer = sc.next();
+			//If the user answers y , correct answer is set to the index of the array possibleAnswers.
+			if (answer.equals("y")) {
+				correctAnswer = i;
+			}
+		}
+		questions.add(new QuestionImpl(question, possibleAnswers, correctAnswer));
+		System.out.print("Type y if you wish to add another question, type anything else if not: ");
+		sc = new Scanner(System.in);
+		String answer = sc.next();
+		//If the user answers y , requestQuestion is called again.
+		if (answer.equals("y")) {
+			requestQuestion(questions);
+		}
+	}
+	
+	public void terminateQuiz(String userName) {
+		System.out.print("Type the ID of the Quiz you wish to terminate: ");
+		Scanner sc = new Scanner(System.in);
+		int quizId = sc.nextInt();
+		try {
+			//Checks whether the supplied quizId exists on the quizService.
+			//If false, terminateQuiz() is called again.
+			if (!service.quizIdExists(quizId)) {
+				System.out.println("Invalid ID, please try again.");
+				terminateQuiz(userName);
+			} else {	
+				service.terminateQuiz(userName, quizId);
+			}
+		} catch (Exception e) {
+				//If an exception occurs, calls chooseOption().
+				System.out.println("There was an error, please try again.");
 		}
 	}
 }
