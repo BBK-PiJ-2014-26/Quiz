dea2ce55fb6f46a2131fdcf30c6a46b4b915d94c
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-25 11:35:07 +0100

    First commit

---
+++ SetupClientTest.java
@@ -0,0 +1,15 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * Tests the interface SetupClient and the class SetupClientImpl.
+ * In order for these tests to function correctly, 
+ * it is assumed that a QuizService is operating on ------.
+ */
+public class SetupClientTest {
+
+	
+}
