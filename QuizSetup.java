e1f907562601794a2cb204f807dd704bf06c69ed
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-25 17:59:45 +0100

    Done.

---
+++ QuizSetup.java
@@ -0,0 +1,16 @@
+/**
+ * Contains the main method to run a SetupClient.
+ */
+public class QuizSetup {
+	
+	/**
+	 *@param args should contain the url of the QuizService.
+	 */
+	public static void main(String[] args) {
+		//Takes the url of the QuizService from the command line.
+		String url = args[0];
+		SetupClient client = new SetupClientImpl();
+		client.launch(url);
+		client.welcome();
+	}
+}
