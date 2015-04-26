4a009561fed31d689043dba7f0e37ecbaef76e8d
gmoore04@dcs.bbk.ac.uk <gmoore04@dcs.bbk.ac.uk>
2015-04-26 16:12:56 +0100

    Reinstated PlayerComparator

---
+++ PlayerComparator.java
@@ -0,0 +1,15 @@
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * A comparator object to be used for organising Players on the QuizService.
+ */
+class PlayerComparator implements Comparator<Player>, Serializable {
+	
+	public int compare(Player p1, Player p2) {
+		String p1UserName = p1.getUserName();
+		String p2UserName = p2.getUserName();
+		return p1UserName.compareTo(p2UserName);
+	}
+}
